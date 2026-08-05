af6c861071598013aba4adeafff2f2c5c69e1f43
Belal-C101 <belaltamerhegab@gmail.com>
2026-08-05 14:12:53 +0300

    feat: add getAll, getById, and deleteById functions to shipment controller and update routes

--- src/controllers/shipment.controller.js
+++ src/controllers/shipment.controller.js
@@ -16,6 +16,74 @@ const createShipment = async (req, res) => {
   }
 };
 
+const getAll = async (req, res) => {
+  try {
+    const shipment = await Shipment.find(req.body);
+
+    res.status(201).json({
+      status: "found",
+      data: shipment,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+
+    res.status(404).json({
+      status: "not found",
+      message: error.message,
+    });
+  }
+};
+
+const getById = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const shipment = await Shipment.findById(req.body._id);
+
+    res.status(201).json({
+      status: "found",
+      data: shipment,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+
+    res.status(404).json({
+      status: "not found",
+      message: error.message,
+    });
+  }
+};
+
+const deleteById = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const shipment = await Shipment.findByIdAndDelete(req.body._id);
+
+    res.status(201).json({
+      status: "deleted",
+      data: shipment,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+
+    res.status(404).json({
+      status: "not found",
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createShipment,
+  getAll,
+  getById,
+  deleteById,
 };
--- src/routes/shipment.routes.js
+++ src/routes/shipment.routes.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const {
   createShipment,
+  getAll,
+  getById,
+  deleteById,
 } = require("../controllers/shipment.controller");
 
 const router = express.Router();
 
 router.post("/", createShipment);
+router.get("/", getAll);
+router.get("/", getById);
+router.delete("/", deleteById);
 
 module.exports = router;
